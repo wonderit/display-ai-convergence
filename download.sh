8d4cbcac2a91accd719dbb7b075b10b224af5b42
WonderIT <kws42590@gmail.com>
2021-07-18 04:04:30 +0900

    gan added

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Download Data
+curl -L -o maxwellfdfd.zip https://drive.google.com/uc?id=14-Bl89OzRtLM1MCW2H81Xvivq8EvTrmB
+mkdir maxwellfdfd
+unzip maxwellfdfd.zip -d ./maxwellfdfd
