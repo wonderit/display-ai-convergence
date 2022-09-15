9e734f042d39b4766a5ecc3fb29be2f9b8b6ecad
WonderIT <won425@korea.ac.kr>
2022-09-15 15:17:49 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Download Data
-curl -L -o maxwellfdfd.zip https://drive.google.com/uc?id=14-Bl89OzRtLM1MCW2H81Xvivq8EvTrmB
+curl -L -o maxwellfdfd.zip https://drive.google.com/uc?id=1Gs9Per_unwdmlXufDxmYEgLAve0ep8Xx
 mkdir maxwellfdfd
 unzip maxwellfdfd.zip -d ./maxwellfdfd
